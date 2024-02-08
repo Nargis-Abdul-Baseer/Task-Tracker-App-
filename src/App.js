f60fbcf188ff0f9f3d8dbe64346c0477f90f0df9
Nargis-Abdul-Baseer <130272364+Nargis-Abdul-Baseer@users.noreply.github.com>
2024-02-09 01:12:17 +0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { TaskTrackerWrapper } from './components/TaskTrackerWrapper';
+
+function App() {
+  return (
+    <div className="App">
+      <TaskTrackerWrapper />
+    </div>
+  );
+}
+
+export default App;
